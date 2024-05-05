cae7c82f9c2f22bd14c279361887957e71b67d37
unknown <hoseinia0870@gmail.com>
2024-05-05 22:23:05 +0330

    last computer update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ShopItemComponent } from './shop-item/shop-item.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ShopItemComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/home/shop-item/shop-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './shop-item.component.html',
+  styleUrl: './shop-item.component.css'
+})
+export class ShopItemComponent {
+
+}

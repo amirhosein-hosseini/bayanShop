2f3ccb740d0843c94061ecd95237b92ddf25e5ad
amirhossein <hoseinia0870@gmail.com>
2024-05-06 13:42:32 +0330

    single Shop page

--- src/app/shop/single-shop/single-shop.component.ts
+++ src/app/shop/single-shop/single-shop.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-single-shop',
   standalone: true,
   imports: [],
+    host: {
+    'class': 'no-header'
+  },
   templateUrl: './single-shop.component.html',
   styleUrl: './single-shop.component.css'
 })

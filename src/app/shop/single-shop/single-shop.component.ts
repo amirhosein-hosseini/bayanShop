a395dbe37b7badffb18221060f620bef6a5a4674
amirhossein <hoseinia0870@gmail.com>
2024-05-15 14:20:36 +0330

    singleShop and basket component

--- src/app/shop/single-shop/single-shop.component.ts
+++ src/app/shop/single-shop/single-shop.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderService } from '../../layout/header/header.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'; // Import BreakpointObserver and Breakpoints
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-single-shop',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
     host: {
     'class': 'no-header'
   },
@@ -15,14 +18,24 @@ import { HeaderService } from '../../layout/header/header.service';
 export class SingleShopComponent {
 
 
-  constructor(public headerService: HeaderService) {
+  constructor(public headerService: HeaderService , private breakpointObserver: BreakpointObserver) {
     this.headerService.shouldDisplayHeader = false; // Set shouldDisplayHeader to false for Singlepage component
   }
 
 
   activeSize: string = ''; // Property to store the active size
+  isMobile: boolean = false;
 
   setActive(size: string): void {
     this.activeSize = size;
+  }
+
+  ngOnInit() {
+    this.breakpointObserver.observe([
+      Breakpoints.Handset,
+      Breakpoints.Small
+    ]).subscribe(result => {
+      this.isMobile = result.matches;
+    });
   }
 }
